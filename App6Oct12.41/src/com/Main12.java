1fe0c52d0f84f4da4b3c59ddca874f683827e3ee
Akash <Akash@192.168.0.13>
2017-10-06 13:46:48 +0530

    added delete

--- App6Oct12.41/src/com/Main12.java
+++ App6Oct12.41/src/com/Main12.java
@@ -4,9 +4,9 @@ public class Main12 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello1 world /!!");
+		System.out.println("Hello1 world /!");
 
-		System.out.println("Hellobye");
+		System.out.println("Hellobye");;
 	}
 
 }
